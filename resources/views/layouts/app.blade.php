090f665486fe4d125dd1ef76f381dbf6f303db91
Pedro Leandro Gomes da Silva <pedroleandrogomesdasilva@outlook.com>
2026-06-01 10:22:28 -0300

    feat: configurar layout principal com header fixo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,21 +4,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Clube Caxiense de Xadrez') }}</title>
-
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
 
-@include('components.site.topbar')
+{{-- Header fixo --}}
+<div class="fixed top-0 left-0 right-0 z-50">
+    @include('components.site.topbar')
+    @include('components.site.navbar')
+</div>
 
 <main>
     @yield('content')
 </main>
+
+@include('components.site.footer')
 
 </body>
 </html>
